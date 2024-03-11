b9a1735d8e1d4d9f813b42e52ededec8c703acd6
Lam Anh <2154050008anh@ou.edu.vn>
2024-03-11 23:27:49 +0700

    test1

--- thongbao.cpp
+++ thongbao.cpp
@@ -4,5 +4,5 @@ int main() {
  cout << "Hello World!\n";
 cout<<"--------------------------\n";
 cout<<"Chương trình này được đăng tại Freetuts.net";
-cout<<"Chương trình này được đăng tại Freetuts.net feature-new change";
+cout<<"Chương trình này được đăng tại Freetuts.net feature-new test 1";
 }
